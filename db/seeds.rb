2f4f1bb6c4e29262e9bc2ec26eba67b84978e889
David Rossiter <david_rossiter@hotmail.com>
2017-11-08 16:37:49 +0000

    Completed yelp without any style

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+
+restaurants_attributes = [
+  {
+    name: 'Dishoom',
+    address: '7 Boundary St, London E2 7JE',
+    phone_number: '07835328299',
+    category: 'french'
+  },
+  {
+    name: 'Pizza East',
+    address: '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '07835328299',
+    category: 'french'
+  },
+  {
+    name: 'Flat Iron',
+    address: 'Rivington Street London E2 123',
+    phone_number: '07835328299',
+    category: 'belgian'
+  },
+  {
+    name: 'Herman ze German',
+    address: '33 Old Compton Street',
+    phone_number: '07835328299',
+    category: 'chinese'
+  }
+]
+Restaurant.create!(restaurants_attributes)
